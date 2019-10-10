52e66564260140daff31a38479b5ddd12f5aca6d
Will Spencer <wspencer@elevate.com>
2019-10-10 15:12:04 -0500

    some major updates

---
+++ TodoApi/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Domain.Models;
+using TodoApi.Domain.Repositories;
+using TodoApi.Persistence.Contexts;
+
+namespace TodoApi.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
