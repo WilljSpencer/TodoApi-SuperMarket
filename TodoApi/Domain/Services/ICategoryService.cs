9d3c069ce34daafc31e188fa536deb2037c8f874
Will Spencer <wspencer@elevate.com>
2019-10-10 08:58:10 -0500

    basic changes

---
+++ TodoApi/Domain/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Domain.Models;
+
+namespace TodoApi.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
